e1fafe249bcbd14bd08fa02706622dfe2212a9a6
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-10-26 01:22:19 +0800

    fixed iteration bug

--- src/Candle.sol
+++ src/Candle.sol
@@ -154,9 +154,9 @@ contract Candle is VRFConsumerBase, DSMath, IERC721Receiver {
         uint closing = randomness % add(sub(a.finalBlock, a.closingBlock), 1);
 	a.finalBlock = 0;
         // work backwards from the closing block until we reach a highest bidder
-        for (uint b = closing; b >= 0; b--) {
-            if (a.highestBidderAtIndex[b] != address(0)) {
-		a.currentHighestBidder = a.highestBidderAtIndex[b];
+        for (uint b = closing + 1; b > 0; b--) {
+            if (a.highestBidderAtIndex[b-1] != address(0)) {
+		a.currentHighestBidder = a.highestBidderAtIndex[b-1];
 		uint winningBidAmount = a.cumululativeBidFromBidder[a.currentHighestBidder];
 		emit AuctionFinalised(auctionId, a.currentHighestBidder, winningBidAmount);
 		return;
