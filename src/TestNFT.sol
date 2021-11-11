2ed91240b716d51c1f117a75cebee3109dd562e8
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-11-11 23:56:40 +0800

    added uri to testnft

--- src/TestNFT.sol
+++ src/TestNFT.sol
@@ -1,22 +1,27 @@
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract TestNFT is ERC721 {
+contract TestNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
 
-    constructor() ERC721("TestNFT", "TNFT") {
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://";
     }
 
+    constructor() ERC721("piccAIso", "CPPN") {
+    }
+
+
     // returns tokenid
-    function mint(address sender) public returns (uint256) {
+    function mint(address sender, string memory metadataURI) public returns (uint256) {
         _tokenId.increment();
         uint256 newItemId = _tokenId.current();
         _mint(sender, newItemId);
-	//_setTokenURI(newItemId, metadataURI);
+        _setTokenURI(newItemId, metadataURI);
         return newItemId;
     }
 }
